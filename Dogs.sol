3c71da4eb0feee52bac3d66d193d0764cf4d4a8b
Frank Kong <frankkongnj@gmail.com>
2023-02-01 17:25:44 +0800

    setup forp 2-random-nft

--- Dogs.sol
+++ Dogs.sol
@@ -6,29 +6,51 @@ import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/utils/Counters.sol";
+import "@chainlink/contracts/src/v0.8/interfaces/VRFCoordinatorV2Interface.sol";
+import "@chainlink/contracts/src/v0.8/VRFConsumerBaseV2.sol";
 
-contract Dogs is ERC721, ERC721Enumerable, ERC721URIStorage, Ownable {
+contract Dogs is ERC721, ERC721Enumerable, ERC721URIStorage, Ownable, VRFConsumerBaseV2 {
     using Counters for Counters.Counter;
     
-    uint256 public MAX_AMOUNT = 3;
-    string uri = "";
+    // Config of NFT
+    uint256 public MAX_AMOUNT = 6;
     Counters.Counter private _tokenIdCounter;
     mapping(address => bool) public whiteList;
     bool public preMintWindow = false;
     bool public mintWindow = false;
 
-    constructor() ERC721("Dogs", "DGS") {}
+    // METADATA of NFT
+    string constant METADATA_SHIBAINU = "ipfs://QmXw7TEAJWKjKifvLE25Z9yjvowWk2NWY3WgnZPUto9XoA";
+    string constant METADATA_HUSKY = "ipfs://QmTFXZBmmnSANGRGhRVoahTTVPJyGaWum8D3YicJQmG97m";
+    string constant METADATA_BULLDOG = "ipfs://QmSM5h4WseQWATNhFWeCbqCTAGJCZc11Sa1P5gaXk38ybT";
+    string constant METADATA_SHEPHERD = "ipfs://QmRGryH7a1SLyTccZdnatjFpKeaydJcpvQKeQTzQgEp9eK";    
+
+    // config of chainlink VRF
+    
+    VRFCoordinatorV2Interface COORDINATOR;
+    uint64 s_subscriptionId;
+    bytes32 keyHash =
+        0x79d3d8832d904592c0bf9818b621522c988bb8b0c05cdc3b15aea1b6e8db0c15;
+    uint32 callbackGasLimit = 100000;
+    uint16 requestConfirmations = 3;
+    uint32 numWords = 1;
+    mapping(uint256 => uint256) reqIdToTokenId;
+
+    constructor(uint64 subId) ERC721("Dogs", "DGS") VRFConsumerBaseV2(0x2Ca8E0C643bDe4C2E08ab1fA0da3401AdAD7734D) {
+        s_subscriptionId = subId;
+        COORDINATOR = VRFCoordinatorV2Interface(0x2Ca8E0C643bDe4C2E08ab1fA0da3401AdAD7734D);
+    }
 
     function preMint() public payable {
         require(preMintWindow, "Premint is not open yet!");
-        require(msg.value == 0.001 ether, "The price of dog nft is 0.001 ether");
+        require(msg.value == 0.001 ether, "The price of dog nft is 0.005 ether");
         require(whiteList[msg.sender], "You are not in the white list");
         require(balanceOf(msg.sender) < 1, "Max amount of NFT minted by an addresss is 1");
         require(totalSupply() < MAX_AMOUNT, "Dog NFT is sold out!");
         uint256 tokenId = _tokenIdCounter.current();
         _tokenIdCounter.increment();
         _safeMint(msg.sender, tokenId);
-        _setTokenURI(tokenId, uri);
+        request(tokenId);
     } 
 
     function mint() public payable {
@@ -38,7 +60,43 @@ contract Dogs is ERC721, ERC721Enumerable, ERC721URIStorage, Ownable {
         uint256 tokenId = _tokenIdCounter.current();
         _tokenIdCounter.increment();
         _safeMint(msg.sender, tokenId);
-        _setTokenURI(tokenId, uri);
+        request(tokenId);
+    }
+
+    function withdraw(address addr) external onlyOwner {
+        payable(addr).transfer(address(this).balance);
+    }
+
+   function request(uint256 _tokenId)
+        internal
+        returns (uint256 requestId)
+    {
+        // Will revert if subscription is not set and funded.
+        requestId = COORDINATOR.requestRandomWords(
+            keyHash,
+            s_subscriptionId,
+            requestConfirmations,
+            callbackGasLimit,
+            numWords
+        );
+        reqIdToTokenId[requestId] = _tokenId;
+        return requestId;
+    }
+
+    function fulfillRandomWords(
+        uint256 _requestId,
+        uint256[] memory _randomWords
+    ) internal override {
+        uint256 randomNumber = _randomWords[0] % 4;
+        if (randomNumber == 0) {
+            _setTokenURI(reqIdToTokenId[_requestId], METADATA_SHIBAINU);
+        } else if (randomNumber == 1) {
+            _setTokenURI(reqIdToTokenId[_requestId], METADATA_HUSKY);
+        } else if (randomNumber == 2) {
+            _setTokenURI(reqIdToTokenId[_requestId], METADATA_SHEPHERD);
+        } else {
+            _setTokenURI(reqIdToTokenId[_requestId], METADATA_BULLDOG);
+        }
     }
 
     function addToWhiteList(address[] calldata addrs) public onlyOwner {
